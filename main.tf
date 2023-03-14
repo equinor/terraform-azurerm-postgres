ffe1bc34e5807fad96fe6703fe44714dbbad17df
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-03-14 13:34:17 +0100

    feat: set firewall rules

--- main.tf
+++ main.tf
@@ -44,6 +44,16 @@ resource "azurerm_postgresql_server" "this" {
   }
 }
 
+resource "azurerm_postgresql_firewall_rule" "this" {
+  for_each = var.firewall_rules
+
+  name                = each.value["name"]
+  resource_group_name = var.resource_group_name
+  server_name         = azurerm_postgresql_server.this.name
+  start_ip_address    = each.value["start_ip_address"]
+  end_ip_address      = each.value["end_ip_address"]
+}
+
 resource "azurerm_postgresql_database" "this" {
   name                = var.database_name
   resource_group_name = var.resource_group_name
