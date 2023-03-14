ecce5225e466d7acf648662f520445c32c4091bd
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-03-14 09:49:30 +0100

    feat: create PostgreSQL server

--- main.tf
+++ main.tf
@@ -1,1 +1,38 @@
+resource "random_password" "this" {
+  length      = 128
+  lower       = true
+  upper       = true
+  numeric     = true
+  special     = true
+  min_lower   = 1
+  min_upper   = 1
+  min_numeric = 1
+  min_special = 1
+}
 
+resource "azurerm_postgresql_server" "this" {
+  name                = var.server_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  administrator_login          = var.administrator_login
+  administrator_login_password = random_password.this.result
+
+  sku_name   = var.sku_name
+  version    = var.version
+  storage_mb = var.storage_mb
+
+  backup_retention_days        = var.backup_retention_days
+  geo_redundant_backup_enabled = var.geo_redundant_backup_enabled
+  auto_grow_enabled            = var.auto_grow_enabled
+
+  # This property is currently still in development and not supported by Microsoft.
+  # It is strongly suggested to leave this value false as not doing so can lead to unclear error messages
+  infrastructure_encryption_enabled = false
+
+  public_network_access_enabled    = var.public_network_access_enabled
+  ssl_enforcement_enabled          = var.ssl_enforcement_enabled
+  ssl_minimal_tls_version_enforced = var.ssl_minimal_tls_version_enforced
+
+  tags = var.tags
+}
--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
+variable "server_name" {
+  description = "The name of this PostgreSQL server."
+  type        = string
+}
+
 variable "resource_group_name" {
   description = "The name of the resource group to create the resources in."
   type        = string
@@ -6,6 +11,65 @@ variable "resource_group_name" {
 variable "location" {
   description = "The location to create the resources in."
   type        = string
+}
+
+variable "administrator_login" {
+  description = "The administrator login of this PostgreSQL server."
+  type        = string
+}
+
+variable "sku_name" {
+  description = "The SKU name for this PostgreSQL server (tier + family + cores)."
+  type        = string
+  default     = "B_Gen4_1"
+}
+
+variable "storage_mb" {
+  description = "The max storage allowed for this PostgreSQL server."
+  type        = number
+  default     = 5120
+}
+
+variable "version" {
+  description = "The version of PostgreSQL to use."
+  type        = string
+  default     = "11"
+}
+
+variable "backup_retention_days" {
+  description = "The number of days that backups should be retained for this PostgreSQL server."
+  type        = number
+  default     = 7
+}
+
+variable "geo_redundant_backup_enabled" {
+  description = "Should geo-redundant backups be enabled for this PostgreSQL server?"
+  type        = bool
+  default     = false
+}
+
+variable "auto_grow_enabled" {
+  description = "Should storage auto grow be enabled for this PostgreSQL server?"
+  type        = bool
+  default     = true # ???
+}
+
+variable "public_network_access_enabled" {
+  description = "Should public network access be enabled for this PostgreSQL server?"
+  type        = bool
+  default     = true
+}
+
+variable "ssl_enforcement_enabled" {
+  description = "Should SSL be enforced on connections?"
+  type        = bool
+  default     = true
+}
+
+variable "ssl_minimal_tls_version_enforced" {
+  description = "The minimum TLS version to support on this PostgreSQL server."
+  type        = string
+  default     = "TLS1_2"
 }
 
 variable "tags" {
