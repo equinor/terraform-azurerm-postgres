a5e30da26843636a111a5461e5220f148ebfb480
Svein Erik Lundervik Fornes <41449429+sefornes@users.noreply.github.com>
2026-01-02 13:10:40 +0100

    feat: update outputs

--- outputs.tf
+++ outputs.tf
@@ -10,21 +10,21 @@ output "database_name" {
 
 output "server_id" {
   description = "The ID of this PostgreSQL server."
-  value       = azurerm_postgresql_server.this.id
+  value       = azurerm_postgresql_flexible_server.this.id
 }
 
 output "server_name" {
   description = "The name of this PostgreSQL server."
-  value       = azurerm_postgresql_server.this.name
+  value       = azurerm_postgresql_flexible_server.this.name
 }
 
 output "administrator_login" {
   description = "The administrator login of this PostgreSQL server."
-  value       = azurerm_postgresql_server.this.name
+  value       = azurerm_postgresql_flexible_server.this.administrator_login
 }
 
 output "administrator_login_password" {
   description = "The administrator login password of this PostgreSQL server."
-  value       = azurerm_postgresql_server.this.administrator_login_password
+  value       = azurerm_postgresql_flexible_server.this.administrator_login_password
   sensitive   = true
 }
